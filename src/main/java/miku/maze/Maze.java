bc285987a11e8d9ee2eef4b80b8f96d7f67441dd
root <herobrinetlb@gmail.com>
2023-08-06 14:20:26 +0800

    变更

--- src/main/java/miku/maze/Maze.java
+++ src/main/java/miku/maze/Maze.java
@@ -23,7 +23,7 @@ public class Maze {
 
     public static final String MOD_ID = "maze";
     public static final String MOD_NAME = "Maze";
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.6";
 
     @SidedProxy(
             clientSide = "miku.maze.proxy.ClientProxy",
